fa45a4fb5d39754a2ccdf534b89bdb5f1936d896
Robert Payne <robertpayne@me.com>
2017-08-16 02:12:23 +1200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "CLibreSSL",
+    pkgConfig: "libtls",
+    providers: [
+        .brew(["libressl"]),
+        .apt(["libressl"]),
+    ]
+)
